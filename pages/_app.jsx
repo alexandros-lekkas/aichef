9afbec0b3ed6a4aa9c9c9d66a22f13bd79c55e5e
Alexandros Lekkas (E-food) <alexandros.f.lekkas@gmail.com>
2023-07-04 15:29:22 +0300

    Added global css and _app (for global styling)

---
+++ pages/_app.jsx
@@ -0,0 +1,14 @@
+                            
+//           /\                
+//          /  \   _ __  _ __  
+//         / /\ \ | '_ \| '_ \ 
+//        / ____ \| |_) | |_) |
+//       /_/    \_\ .__/| .__/ 
+//   ______       | |   | |    
+//  |______|      |_|   |_|    
+
+import '../styles/global.css';
+
+export default function App({ Component, pageProps}) {
+    return <Component {...pageProps} />;
+}
