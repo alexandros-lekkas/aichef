49ac5c302fc8b43d205586d5dbe74c456ae83661
Alexandros Lekkas (E-food) <alexandros.f.lekkas@gmail.com>
2023-07-04 16:35:36 +0300

    Chat modified

--- api/model/chat.jsx
+++ api/model/chat.jsx
@@ -1,27 +1,51 @@
+//   _____ _           _   
+//  / ____| |         | |  
+// | |    | |__   __ _| |_ 
+// | |    | '_ \ / _` | __|
+// | |____| | | | (_| | |_ 
+//  \_____|_| |_|\__,_|\__|     
+//
+// Recipe's generated through straight chat answers
+// (not using pre-trained model)              
+
+// OpenAI
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
 
-// Create an OpenAI API client (that's edge friendly!)
+// Create an OpenAI API client
 const config = new Configuration({
   apiKey: process.env.OPENAI_API_KEY
 })
 const openai = new OpenAIApi(config)
- 
-// IMPORTANT! Set the runtime to edge
-export const runtime = 'edge'
- 
-export async function POST(req) {
-  // Extract the `messages` from the body of the request
-  const { messages } = await req.json()
- 
-  // Ask OpenAI for a streaming chat completion given the prompt
-  const response = await openai.createChatCompletion({
-    model: 'gpt-3.5-turbo',
-    stream: true,
-    messages
-  })
-  // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response)
-  // Respond with the stream
-  return new StreamingTextResponse(stream)
-}
+
+// Send message to GPT3 function
+export async function sendMessage(message) {
+    console.log('[CHAT] Prompt:', message)
+
+    // Array of messages constructed by user array
+    const messages = [
+        {
+            role: 'system',
+            content: 'You are a helpful assistant that provides recipe suggestions'
+        },
+        {
+            role: 'user',
+            content: message // Message is added from the arguments passed into the function
+        }
+    ];
+
+    try {
+        // Request chat completion from OpenAI using user's prompt
+        const response = await openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages,
+        });
+
+        // Reponse handled with appplication logic
+        const reply = response.choices[0].message.content;
+        console.log('[CHAT] Reply:', reply);
+    } catch (error) {
+        console.error('[ERROR]', error)
+    }
+
+};
--- pages/index.jsx
+++ pages/index.jsx
@@ -4,11 +4,35 @@
 //   | | | '_ \ / _` |/ _ \ \/ /
 //  _| |_| | | | (_| |  __/>  < 
 // |_____|_| |_|\__,_|\___/_/\_\
-                            
+
+// React
+import React, { useState } from 'react';
+
+// Next
 import Head from 'next/head';
+
+// Components
 import Layout from '../components/layout/layout.jsx';
 
+// API
+import { sendMessage } from '../api/model/chat.jsx';
+
+// Home
 export default function Home() {
+    const [message, setMessage] = useState('');
+
+    // Change value on input
+    const handleInputChange = (event) => {
+        setMessage(event.target.value); // Set value to response
+    };
+
+    // POST to chatGPT on submit
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        sendMessage(message);
+        setMessage('');
+    };
+
     return (
         <Layout home>
             <Head>
@@ -16,6 +40,10 @@ export default function Home() {
             </Head>
             <section>
                 <p>👨‍🍳 OpenAI powered recipe and image generation</p>
+            </section>
+            <section>
+                <input type="text" value={message} onChange={handleInputChange}/>
+                <button type="submit">Send</button>
             </section>
         </Layout>
     );
