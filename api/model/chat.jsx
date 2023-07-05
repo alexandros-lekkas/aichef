6ce3f50057c7e526537840d5b1f7e9724e4e0a69
Alexandros Lekkas (E-food) <alexandros.f.lekkas@gmail.com>
2023-07-05 10:18:36 +0300

    Using Vercel SDK now

--- api/model/chat.jsx
+++ api/model/chat.jsx
@@ -1,54 +1,46 @@
-//   _____ _           _   
-//  / ____| |         | |  
-// | |    | |__   __ _| |_ 
+//   _____ _           _
+//  / ____| |         | |
+// | |    | |__   __ _| |_
 // | |    | '_ \ / _` | __|
-// | |____| | | | (_| | |_ 
-//  \_____|_| |_|\__,_|\__|     
+// | |____| | | | (_| | |_
+//  \_____|_| |_|\__,_|\__|
 //
 // Recipe's generated through straight chat answers
-// (not using pre-trained model)              
+// (not using pre-trained model)
 
-// OpenAI
-import { OpenAIStream, StreamingTextResponse } from 'ai';
-import { Configuration, OpenAIApi } from 'openai-edge';
+// OpenAI Imports
+import { OpenAIStream, StreamingTextResponse } from "ai";
+import { Configuration, OpenAIApi } from "openai-edge";
 
-// Create an OpenAI API client
+// OpebAI Configuration
 const config = new Configuration({
-  apiKey: "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"
+    apiKey: "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",
 });
-const openai = new OpenAIApi(config)
-const key = process.env.OPENAI_API_KEY;
+const openai = new OpenAIApi(config);
+export const runtime = "edge";
 
-// Send message to GPT3 function
+// Send Message to OpenAI Function
 export async function sendMessage(message) {
-    console.log('[Chat] Prompt:', message)
-    console.log('Key', key);
-
-    // Array of messages constructed by user array
+    // Message
     const messages = [
         {
-            role: 'system',
-            content: 'You are a helpful assistant that provides recipe suggestions'
+            role: "system",
+            content: "You are a helpful assistant that provides recipe suggestions",
         },
         {
-            role: 'user',
-            content: message // Message is added from the arguments passed into the function
-        }
+            role: "user",
+            content: message,
+        },
     ];
 
-    try {
-        // Request chat completion from OpenAI using user's prompt
-        const response = await openai.createChatCompletion({
-            model: 'gpt-3.5-turbo',
-            messages
-        });
-        console.log('[Chat] Response:', response);
+    // Grabbing Streaming Response
+    const response = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        stream: true,
+        messages,
+    });
 
-        // Reponse handled with appplication logic
-        const reply = response.choices[0].message.content;
-        console.log('[Chat] Reply:', reply);
-    } catch (error) {
-        console.error('[Reply]', error)
-    }
-
-};
+    // Converting into text-friendly stream and returning it
+    const stream = OpenAIStream(response);
+    return new StreamingTextResponse(stream);
+}
